fcf6c4d4b5b10e995f05e017fc7db9bd6d25029a
rasmaninov <Rahsaan@live.nl>
2024-01-24 14:32:45 +0100

    Update MinimalWorkingCode.ino

--- MinimalWorkingCode/MinimalWorkingCode.ino
+++ MinimalWorkingCode/MinimalWorkingCode.ino
@@ -1,30 +1,20 @@
 const int amount = 8;
-float smoothingFactor = 0.09;
 
 const int sensorPin[amount] = {16, 17, 18, 19, 20, 21, 22, 23};
 int sensorValue[amount] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-int sensorMax
-int sensorMin 
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-
-  for (int i = 0; i < amount; i++) {
-    //read sensordata
-    sensorValue[i] = analogRead(sensorPin[i]);
-  };
-
   //  print sensor data
   for (int i = 0; i < amount; i++) {
+    sensorValue[i] = analogRead(sensorPin[i]);
     Serial.print(" ");
     Serial.print(sensorValue[i]);
   };
   Serial.println();
-
 
   delay(20);
 }
