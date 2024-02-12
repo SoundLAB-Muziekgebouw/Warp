cbe71520f03d82cf583f9b5d7b05ea958c87ddf4
rasmaninov <90449830+rasmaninov@users.noreply.github.com>
2024-02-12 13:19:46 +0100

    Update SoundlabNew.ino
    Remove rubbish encoderCode

--- SoundlabNewWIP/SoundlabNew/SoundlabNew.ino
+++ SoundlabNewWIP/SoundlabNew/SoundlabNew.ino
@@ -40,65 +40,17 @@ String onOff;
 
 int readingButton;
 
-//copy paste hihi
-const int pinA = 12;           // Our first hardware interrupt pin is digital pin 12
-const int pinB = 11;           // Our second hardware interrupt pin is digital pin 13
-volatile byte aFlag = 0;       // let's us know when we're expecting a rising edge on pinA to signal that the encoder has arrived at a detent
-volatile byte bFlag = 0;       // let's us know when we're expecting a rising edge on pinB to signal that the encoder has arrived at a detent (opposite direction to when aFlag is set)
-volatile byte encoderPos = 0;  //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
-volatile byte oldEncPos = 0;   //stores the last encoder position value so we can compare to the current reading and see if it has changed (so we know when to print to the serial monitor)
-volatile byte reading = 0;     //somewhere to store the direct values we read from our interrupt pins before checking to see if we have moved a whole detent
-//popy caste hihi
 
 void setup() {
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
   pinMode(BUTTON2_PIN, INPUT_PULLUP);
-  //copy paste hihi
-  pinMode(pinA, INPUT_PULLUP);       // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
-  pinMode(pinB, INPUT_PULLUP);       // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
-  attachInterrupt(0, PinA, RISING);  // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
-  attachInterrupt(1, PinB, RISING);
-  //popy caste hihi
+
   Serial.begin(115200);
 
   //calibrate()
 }
 
-void PinA(){
-  cli(); //stop interrupts happening before we read pin values
-  reading = PIND & 0xC; // read all eight pin values then strip away all but pinA and pinB's values
-  if(reading == B00001100 && aFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
-    encoderPos --; //decrement the encoder's position count
-    bFlag = 0; //reset flags for the next turn
-    aFlag = 0; //reset flags for the next turn
-  }
-  else if (reading == B00000100) bFlag = 1; //signal that we're expecting pinB to signal the transition to detent from free rotation
-  sei(); //restart interrupts
-}
-
-void PinB(){
-  cli(); //stop interrupts happening before we read pin values
-  reading = PIND & 0xC; //read all eight pin values then strip away all but pinA and pinB's values
-  if (reading == B00001100 && bFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
-    encoderPos ++; //increment the encoder's position count
-    bFlag = 0; //reset flags for the next turn
-    aFlag = 0; //reset flags for the next turn
-  }
-  else if (reading == B00001000) aFlag = 1; //signal that we're expecting pinA to signal the transition to detent from free rotation
-  sei(); //restart interrupts
-}
-
-
 void loop() {
-
-    Serial.print("encody ");
-    Serial.println(encoderPos);
- if(oldEncPos != encoderPos) {
-
-    oldEncPos = encoderPos;
-  }
-
-
   for (int i = 0; i < amount; i++) {
     //read sensordata
     sensorValue[i] = analogRead(sensorPin[i]);
@@ -118,8 +70,6 @@ void loop() {
   Serial.print(knopCheck(BUTTON1_PIN, 1));
   Serial.print("  ");
   Serial.println(knopCheck(BUTTON2_PIN, 2));
-
-
 
   /*if(buttoncheck){
     led op rood
@@ -141,8 +91,6 @@ void loop() {
     buttoncheck = true
   }
      */
-
-
 
   delay(20);
 }
